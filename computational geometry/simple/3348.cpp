302366d4a2c2e52324d90f8e11860f3de7e2d79a
alexma <alexmaruichen@hotmail.com>
2017-03-04 13:50:54 +0800

    Update categories for computational geography

---
+++ computational geometry/simple/3348.cpp
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+#include <algorithm>
+
+using namespace std;
+
+const int maxn = 1e4 + 10;
+
+struct point
+{
+	double x, y;
+	point() {}
+	point(double x, double y) :x(x), y(y) {}
+}
+
+int main()
+{
+
+}
