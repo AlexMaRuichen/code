4d460da61ef219c2c47148dd19a7c684b949eda9
alexma <alexmaruichen@hotmail.com>
2017-03-16 18:06:24 +0800

    Update delete.bat

--- delete.bat
+++ delete.bat
@@ -1,21 +1,19 @@
 @echo off
-echo WARNING : this batch will delete all the *.exe *.in *.out recursively
-set /p read=Press Y to continue:
-if /i %read% EQU Y (
-	for /r %%i in (*.exe, *.in, *.out) do (
-		echo %%i
-	)
-) else (
-	echo Canceled 
+
+echo                       		     WARNING : you will delete the files below! 
+
+for /r %%i in (*.exe, *.in, *.out) do (
+	echo %%i
 )
-set /p A=Press Y to delete:
-if /i %A% EQU Y (
+set /p read=Do you really want to delete the files above ? (y / n)  
+
+if /i %read% EQU Y (
 	for /r %%i in (*.exe, *.in, *.out) do (
 		del "%%i"
 		echo %%i deleted
 	)
-	echo Done
+	echo done
 ) else (
 	echo Canceled
 )
-pause
+timeout /t 1 > nul
