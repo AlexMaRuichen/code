f124ef0fab6934a4a928ba33a10279ff0bc90d6a
alexma <alexmaruichen@hotmail.com>
2017-04-05 21:13:54 +0800

    Update clang-format

--- 2-SAT/1823.cpp
+++ 2-SAT/1823.cpp
@@ -1,12 +1,17 @@
 #include <stdio.h>
-#include <stack>
 #include <string.h>
+
+
 #include <algorithm>
-#define inv(x) x&1?x^1:x|1
+#include <stack>
+
+#define inv(x) x & 1 ? x ^ 1 : x | 1
+
 using namespace std;
-const int maxn=210;
-const int maxm=2010;
-int n,m;
+
+const int maxn = 210;
+const int maxm = 2010;
+int n, m;
 char ch;
 bool flag;
 
@@ -21,85 +26,83 @@ int dfn[maxn];
 int low[maxn];
 bool v[maxn];
 bool ins[maxn];
-stack <int> s;
-inline void dfs(int x)
+stack<int> s;
+inline void dfs(int x) 
 {
-	v[x]=ins[x]=true;
-	dfn[x]=low[x]=++cnt;
-	s.push(x);
-	for(int i=head[x];i;i=next[i])
-		if(!v[to[i]])
-			dfs(to[i]),
-			low[x]=min(low[to[i]],low[x]);
-		else if(ins[to[i]])
-			low[x]=min(dfn[to[i]],low[x]);
-	if(low[x]==dfn[x])
+    v[x] = ins[x] = true;
+    dfn[x] = low[x] = ++cnt;
+    s.push(x);
+    for (int i = head[x]; i; i = next[i])
+        if (!v[to[i]])
+            dfs(to[i]), low[x] = min(low[to[i]], low[x]);
+        else if (ins[to[i]])
+            low[x] = min(dfn[to[i]], low[x]);
+    if (low[x] == dfn[x]) 
 	{
-		int now=0;
-		++scc;
-		while(now^x)
+        int now = 0;
+        ++scc;
+        while (now ^ x) 
 		{
-			now=s.top();
-			s.pop();
-			ins[now]=false;
-			belong[now]=scc;
-		}
-	}
+            now = s.top();
+            s.pop();
+            ins[now] = false;
+            belong[now] = scc;
+        }
+    }
 }
-int main()
+int main() 
 {
-	int T,i;
-	scanf("%d",&T);
-	while(T--)
+    int T, i;
+    scanf("%d", &T);
+    while (T--) 
 	{
-		scanf("%d%d",&n,&m);
-		memset(head,0,sizeof(int)*((n<<1)+5));
-		memset(v,0,sizeof(bool)*((n<<1)+5));
-		tot=0;
-		scc=0;
-		cnt=0;
-		for(i=1;i<=m;i++)
+        scanf("%d%d", &n, &m);
+        memset(head, 0, sizeof(int) * ((n << 1) + 5));
+        memset(v, 0, sizeof(bool) * ((n << 1) + 5));
+        tot = 0;
+        scc = 0;
+        cnt = 0;
+        for (i = 1; i <= m; i++) 
 		{
-			static int x,y;
-			x=0;
-			ch=getchar();
-			while(ch!='m'&&ch!='h')
-				ch=getchar();
-			flag=ch=='m';
-			ch=getchar();
-			while(ch>='0'&&ch<='9')
-				x=(x<<1)+(x<<3)+ch-'0',ch=getchar();
-			x=flag?x<<1:x<<1|1;
+            static int x, y;
+            x = 0;
+            ch = getchar();
+            while (ch != 'm' && ch != 'h')
+                ch = getchar();
+            flag = ch == 'm';
+            ch = getchar();
+            while (ch >= '0' && ch <= '9')
+                x = (x << 1) + (x << 3) + ch - '0', ch = getchar();
+            x = flag ? x << 1 : x << 1 | 1;
 
-			y=0;
-			ch=getchar();
-			while(ch!='m'&&ch!='h')
-				ch=getchar();
-			flag=ch=='m';
-			ch=getchar();
-			while(ch>='0'&&ch<='9')
-				y=(y<<1)+(y<<3)+ch-'0',ch=getchar();
-			y=flag?y<<1:y<<1|1;
-			
-			to[++tot]=inv(y);
-			next[tot]=head[x];
-			head[x]=tot;
+            y = 0;
+            ch = getchar();
+            while (ch != 'm' && ch != 'h')
+                ch = getchar();
+            flag = ch == 'm';
+            ch = getchar();
+            while (ch >= '0' && ch <= '9')
+                y = (y << 1) + (y << 3) + ch - '0', ch = getchar();
+            y = flag ? y << 1 : y << 1 | 1;
 
-			to[++tot]=inv(x);
-			next[tot]=head[y];
-			head[y]=tot;
+            to[++tot] = inv(y);
+            next[tot] = head[x];
+            head[x] = tot;
 
-		}
-		for(i=2;i<=(n<<1|1);i++)
-			if(!v[i])
-				dfs(i);
-		for(i=1;i<=n;i++)
-			if(belong[i<<1]==belong[i<<1|1])
+            to[++tot] = inv(x);
+            next[tot] = head[y];
+            head[y] = tot;
+        }
+        for (i = 2; i <= (n << 1 | 1); i++)
+            if (!v[i])
+                dfs(i);
+        for (i = 1; i <= n; i++)
+            if (belong[i << 1] == belong[i << 1 | 1]) 
 			{
-				puts("BAD");
-				goto lableA;
-			}
-		puts("GOOD");
-		lableA:;
-	}
+                puts("BAD");
+                goto lableA;
+            }
+        puts("GOOD");
+    lableA:;
+    }
 }
--- 2-SAT/1997.cpp
+++ 2-SAT/1997.cpp
@@ -1,24 +1,28 @@
 #include <stdio.h>
-#include <algorithm>
 #include <string.h>
+
+#include <algorithm>
 #include <stack>
-#define in(i) (i<<1)
-#define out(i) (i<<1|1)
-#define inv(i) (i&1?i^1:i|1)
+
+#define in(i) (i << 1)
+#define out(i) (i << 1 | 1)
+#define inv(i) (i & 1 ? i ^ 1 : i | 1)
+
 using namespace std;
-const int maxn=1210;
-const int maxm=maxn*maxn;
-int n,m;
+
+const int maxn = 1210;
+const int maxm = maxn * maxn;
+int n, m;
 
 int to[maxm];
 int next[maxm];
 int head[maxn];
 int tot;
-void add(int x,int y)
+void add(int x, int y) 
 {
-	to[++tot]=y;
-	next[tot]=head[x];
-	head[x]=tot;
+    to[++tot] = y;
+    next[tot] = head[x];
+    head[x] = tot;
 }
 
 int belong[maxn];
@@ -26,103 +30,101 @@ int dfn[maxn];
 int low[maxn];
 bool v[maxn];
 bool ins[maxn];
-stack <int> s;
+stack<int> s;
 int cnt;
 int scc;
-void dfs(int x)
+void dfs(int x) 
 {
-	dfn[x]=low[x]=++cnt;
-	v[x]=ins[x]=true;
-	s.push(x);
-	for(int i=head[x];i;i=next[i])
-		if(!v[to[i]])
-			dfs(to[i]),
-			low[x]=min(low[to[i]],low[x]);
-		else if(ins[to[i]])
-			low[x]=min(dfn[to[i]],low[x]);
-	if(dfn[x]==low[x])
+    dfn[x] = low[x] = ++cnt;
+    v[x] = ins[x] = true;
+    s.push(x);
+    for (int i = head[x]; i; i = next[i])
+        if (!v[to[i]])
+            dfs(to[i]), low[x] = min(low[to[i]], low[x]);
+        else if (ins[to[i]])
+            low[x] = min(dfn[to[i]], low[x]);
+    if (dfn[x] == low[x]) 
 	{
-		int now=0;
-		++scc;
-		while(now^x)
+        int now = 0;
+        ++scc;
+        while (now ^ x) 
 		{
-			now=s.top();
-			s.pop();
-			ins[now]=false;
-			belong[now]=scc;
-		}
-	}
+            now = s.top();
+            s.pop();
+            ins[now] = false;
+            belong[now] = scc;
+        }
+    }
 }
-void tarjan()
+void tarjan() 
 {
-	for(int i=1;i<=m;i++)
+    for (int i = 1; i <= m; i++) 
 	{
-		if(!v[in(i)])
-			dfs(in(i));
-		if(!v[out(i)])
-			dfs(out(i));
-	}
+        if (!v[in(i)])
+            dfs(in(i));
+        if (!v[out(i)])
+            dfs(out(i));
+    }
 }
 
-void init()
+void init() 
 {
-	memset(head,0,sizeof(head));
-	memset(v,0,sizeof(v));
-	tot=0;
-	cnt=0;
-	scc=0;
+    memset(head, 0, sizeof(head));
+    memset(v, 0, sizeof(v));
+    tot = 0;
+    cnt = 0;
+    scc = 0;
 }
-int x[maxn<<2];
-int y[maxn<<2];
+int x[maxn << 2];
+int y[maxn << 2];
 int pos[maxn];
 int c[maxn];
-void build()
+void build() 
 {
-	for(int i=1;i<=m;i++)
-		for(int j=i+1;j<=m;j++)
-			if((x[j]<x[i]&&y[j]>x[i]&&y[j]<y[i])||(x[j]>x[i]&&x[j]<y[i]&&y[j]>y[i]))
-				add(in(i),out(j)),add(in(j),out(i)),add(out(j),in(i)),add(out(i),in(j));
+    for (int i = 1; i <= m; i++)
+        for (int j = i + 1; j <= m; j++)
+            if ((x[j] < x[i] && y[j] > x[i] && y[j] < y[i]) ||
+                (x[j] > x[i] && x[j] < y[i] && y[j] > y[i]))
+                add(in(i), out(j)), add(in(j), out(i)), add(out(j), in(i)),
+                    add(out(i), in(j));
 }
-bool check()
+bool check() 
 {
-	for(int i=1;i<=m;i++)
-		if(belong[in(i)]==belong[out(i)])
-			return false;
-	return true;
+    for (int i = 1; i <= m; i++)
+        if (belong[in(i)] == belong[out(i)])
+            return false;
+    return true;
 }
-int main()
+int main() 
 {
-	int T;
-	scanf("%d",&T);
-	while(T--)
+    int T;
+    scanf("%d", &T);
+    while (T--) 
 	{
-		scanf("%d%d",&n,&m);
-		if(m>n*3-6)
+        scanf("%d%d", &n, &m);
+        if (m > n * 3 - 6) 
 		{
-			for(int i=1;i<=m;i++)
-				scanf("%*d%*d");
-			for(int i=1;i<=n;i++)
-				scanf("%*d");
-			puts("NO");
-			continue;
-		}
-		init();
-		for(int i=1;i<=m;i++)
-			scanf("%d%d",&x[i],&y[i]);
-		for(int i=1;i<=n;i++)
-			scanf("%d",&c[i]),pos[c[i]]=i;	
-		for(int i=1;i<=m;i++)
+            for (int i = 1; i <= m; i++)
+                scanf("%*d%*d");
+            for (int i = 1; i <= n; i++)
+                scanf("%*d");
+            puts("NO");
+            continue;
+        }
+        init();
+        for (int i = 1; i <= m; i++)
+            scanf("%d%d", &x[i], &y[i]);
+        for (int i = 1; i <= n; i++)
+            scanf("%d", &c[i]), pos[c[i]] = i;
+        for (int i = 1; i <= m; i++) 
 		{
-			x[i]=pos[x[i]];
-			y[i]=pos[y[i]];
-			if(x[i]>y[i])
-				swap(x[i],y[i]);
-		}
-		build();
-		tarjan();
-		if(check())
-			puts("YES");
-		else
-			puts("NO");
-	}
+            x[i] = pos[x[i]];
+            y[i] = pos[y[i]];
+            if (x[i] > y[i])
+                swap(x[i], y[i]);
+        }
+        build();
+        tarjan();
+        puts(check() ? "YES" : "NO");
+    }
 }
