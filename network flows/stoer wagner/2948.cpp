3e3edfa082f555ecf0ebb5c5d760dd9eb63d3893
alexma <alexmaruichen@hotmail.com>
2017-04-15 16:55:03 +0800

    Update 2948.cpp

---
+++ network flows/stoer wagner/2948.cpp
@@ -0,0 +1,70 @@
+#include <stdio.h>
+#include <string.h>
+ 
+#include <algorithm>
+ 
+template <typename T> inline bool tense(T &a, const T &b)
+{
+    return b < a ? a = b, true : false;
+}
+ 
+template <typename T> inline bool relax(T &a, const T &b)
+{
+    return a < b ? a = b, true : false;
+}
+ 
+using namespace std;
+ 
+const int maxn = 510;
+const int inf = 1e9;
+ 
+int map[maxn][maxn];
+int fa[maxn];
+int dis[maxn];
+bool v[maxn];
+ 
+int stoer_wagner(int n)
+{
+    int ans = inf;
+    for (int i = 1; i <= n; i++)
+        fa[i] = i;
+    while (n > 1)
+    {
+        int pre = 1;
+        memset(v, 0, sizeof(v));
+        memset(dis, 0, sizeof(dis));
+        for (int i = 2; i <= n; i++)
+        {
+            int k = 0;
+            for (int j = 2; j <= n; j++)
+                if (!v[fa[j]])
+                {
+                    dis[fa[j]] += map[fa[pre]][fa[j]];
+                    if (!k || dis[fa[k]] < dis[fa[j]])
+                        k = j;
+                }
+            v[fa[k]] = true;
+            if (i == n)
+            {
+                tense(ans, dis[fa[k]]);
+                for (int j = 1; j <= n; j++)
+                    map[fa[pre]][fa[j]] += map[fa[j]][fa[k]],
+                    map[fa[j]][fa[pre]] += map[fa[j]][fa[k]];
+                fa[k] = fa[n--];
+            }
+            pre = k;
+        }
+    }
+    return ans;
+}
+ 
+int main()
+{
+    int n, m;
+    scanf("%d%d", &n, &m);
+    for (int i = 1, x, y, z; i <= m; i++)
+        scanf("%d%d%d", &x, &y, &z),
+        map[x][y] += z,
+        map[y][x] += z;
+    printf("%d\n", stoer_wagner(n));
+}
