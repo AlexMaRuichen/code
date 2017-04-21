c6dce6ffc3945e695d7792b649638111e65c8a43
alexma <alexmaruichen@hotmail.com>
2017-04-21 16:09:11 +0800

    Update 2941.cpp

--- simplex/2941.cpp
+++ simplex/2941.cpp
@@ -3,14 +3,23 @@
 
 #include <algorithm>
 
+template <typename T> inline bool tense(T &a, const T &b)
+{
+	return b < a ? a = b, true : false;
+}
+
+template <typename T> inline bool relax(T &a, const T &b)
+{
+	return a < b ? a = b, true : false;
+}
+
 using namespace std;
 
 const int maxn = 110;
-const double eps = 1e-7;
-const int inf = 1e9;
+const double eps = 1e-8;
+const double inf = 1e100;
 
 int n, m;
-
 double a[maxn][maxn];
 double b[maxn];
 double c[maxn];
@@ -39,30 +48,30 @@ void pivot(int l, int e)
 	c[e] = -c[e] * a[l][e];
 }
 
-double simplex()
+bool simplex()
 {
 	int i, l, e;
 	double tmp;
 	while (true)
 	{
-		for (i = 1; i <= n; i++)
-			if (c[i] > eps)
+		for (e = 1; e <= n; e++)
+			if (c[e] > eps)
 				break;
-		if ((e = i) == n + 1)
-			return v;
+		if (e == n + 1)
+			return true;
 		tmp = inf;
 		for (i = 1; i <= m; i++)
-			if (a[i][e] > eps && b[i] / a[i][e] < tmp)
-				tmp = b[i] / a[i][e], l = i;
-		if (tmp == inf)
-			return inf;
+			if (a[i][e] > eps && tense(tmp, b[i] / a[i][e]))
+				l = i;
+		if (tmp == inf)	
+			return false;
 		pivot(l, e);
 	}
 }
 
 int main()
 {
-	scanf("%d%d", &m, &n);
+	scanf("%d%d", &n, &m);
 	for (int i = 1; i <= m; i++)
 	{
 		for (int j = 1; j <= n; j++)
@@ -71,6 +80,5 @@ int main()
 	}
 	for (int i = 1; i <= n; i++)
 		scanf("%lf", &c[i]);
-	double ans = simplex();
-	ans == inf ? puts("Infinity") : printf("%.2lf\n", ans);
+	simplex() ? printf("%.2lf", v) : puts("Infinity");
 }
